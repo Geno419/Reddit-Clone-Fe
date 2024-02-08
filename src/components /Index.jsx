179949d817be672b6ae2175b34de52908c5d6c2d
Geno419 <ogbeidegild@gmail.com>
2024-02-08 10:43:51 +0000

    Error handling

--- src/components /Index.jsx
+++ src/components /Index.jsx
@@ -2,5 +2,6 @@ import Header from "./Header";
 import AllArticles from "./AllArticles";
 import SingleArticle from "./SingleArticle";
 import Topic from "./Topic";
+import NotFound from "./NotFound";
 
-export { Header, AllArticles, SingleArticle, Topic };
+export { Header, AllArticles, SingleArticle, Topic, NotFound };
